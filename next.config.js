b7a477a9bf2714dd2dcc00d4f1365243605b8b8c
Michael Yakovlev <kvadrofilii@gmail.com>
2023-01-18 23:04:44 +0300

    add svg declare & webpack config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config, options) {
+    config.module.rules.push({
+      loader: '@svgr/webpack',
+      options: {
+        prettier: false,
+        svgo: true,
+        svgoConfig: {
+          plugins: [{ removeViewBox: false }],
+        },
+        titleProp: true,
+      },
+      test: /\.svg$/,
+    });
+
+    return config;
+  },
+};
 
 module.exports = nextConfig
