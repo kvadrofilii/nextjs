c7fd44ebd992c50c0f68daef0f09da7eca102f60
Michael Yakovlev <kvadrofilii@gmail.com>
2023-01-19 07:57:50 +0300

    add Tag component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { Inter } from '@next/font/google';
-import { Htag } from '../components';
+import { Button, Htag, P, Tag } from '../components';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -7,6 +7,20 @@ export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h1">Текст</Htag>
+      <Button appearance="primary" className="test">
+        Button
+      </Button>
+      <Button appearance="ghost" arrow="right">
+        Button
+      </Button>
+      <P size="s">Lorem ipsum dolor sit amet consectetur adipisicing elit.</P>
+      <P>Lorem ipsum dolor sit amet consectetur adipisicing elit.</P>
+      <P size="l">Lorem ipsum dolor sit amet consectetur adipisicing elit.</P>
+      <Tag>Ghost</Tag>
+      <Tag color="primary">Primary</Tag>
+      <Tag color="green" size="m">
+        Green
+      </Tag>
     </>
   );
 }
