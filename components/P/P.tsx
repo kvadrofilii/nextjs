4adc53700b038f7a84f0353916ed165474064f6d
Michael Yakovlev <kvadrofilii@gmail.com>
2023-01-19 07:56:49 +0300

    add P component

---
+++ components/P/P.tsx
@@ -0,0 +1,16 @@
+import { PProps } from './P.props';
+import cn from 'classnames';
+import styles from './P.module.css';
+
+export const P = ({ size = 'm', children, className, ...props }: PProps): JSX.Element => (
+  <p
+    className={cn(styles.p, className, {
+      [styles.s]: size === 's',
+      [styles.m]: size === 'm',
+      [styles.l]: size === 'l',
+    })}
+    {...props}
+  >
+    {children}
+  </p>
+);
